997c9515aa3ec0eb8a89bc0f812a0d090b2890f4
Mike Rightmire <Mike.Rightmire@BiocomSoftware.com>
2017-09-28 09:13:31 +0200

    2017-09-28 MPR - Completed FidArchivableRootDirs.py (beta)

--- FindArchivableRootDirs.py
+++ FindArchivableRootDirs.py
@@ -10,23 +10,23 @@ __status__      = "Development"
 
 
 from argparse       import ArgumentParser
-from common.checks         import Checks
+from common.checks  import Checks
 checks = Checks()
 _delim = checks.directory_deliminator()
 # from common.confighandler  import ConfigHandler # Needs to be updated for Python3
-from common.loghandler     import log
-from common.convert_timestring_input import convert_timestring_input as get_seconds 
+from common.loghandler               import log
+from common.convert_timestring_input import convert_timestring_input as get_time 
 
 import atexit
 import inspect
 import os
 import re
+import time
 
 class Find(object):
     def __init__(self, parser = {}, *args, **kwargs):
         atexit.register(self._cleanup)
         self._set_config(parser, args, kwargs)
-        self.main()
                 
     def _arg_parser(self, parser):
         """
@@ -45,6 +45,14 @@ class Find(object):
             Returns the parser object for later use by argparse
             
         """
+        parser.add_argument('--increment', '-i', 
+                            action="store", dest="INCREMENTOUT", type=str, default = 's', 
+                            help='The time increment for output. CASE SENSITIVE.  \n I.e. \n Y = Years \n M = Months \n d = Days \n h = Hours \n m = Minutes \n s = Seconds  \n H = Human-readable.')
+
+        parser.add_argument('--screen', '-s', 
+                            action="store", dest="TERMINAL", type=bool, default = True, 
+                            help='Set to "True" to dump output to screen. Otherwise False. DEFAULT: True')
+        
         parser.add_argument('--conf', '-c', 
                             action="store", dest="CONFIGFILE", type=str, default = '.', 
                             help='Full path to the config file. DEFAULT: "./FindArchivableRootDirs.conf"')
@@ -62,11 +70,11 @@ class Find(object):
                             help='Type of file system object to use for collecting dates. \n d=directories (only) \n  f=files (only) \n  b=both. DEFAULT: Both')
 
         parser.add_argument('--older', '-O', 
-                            action="store", dest="OLDER", type=str, default = '1d', 
+                            action="store", dest="OLDER", type=str, default = '0', 
                             help='Collect statistics on directories/files older than <int><years/months/days/hours/seconds>. DEFAULT: 1 day')
 
         parser.add_argument('--newer', '-N', 
-                            action="store", dest="NEWER", type=str, default = '1d', 
+                            action="store", dest="NEWER", type=str, default = '0', 
                             help='Collect statistics on directories/files newer than <int><years/months/days/hours/seconds>. DEFAULT: 1 day')
 
         parser.add_argument('--out', '-o', 
@@ -136,11 +144,14 @@ class Find(object):
         self.formatter  = kwargs.pop('formatter', '%(asctime)s-%(name)s-%(levelname)s-%(message)s')
         self.create_paths = kwargs.pop('create_paths', True) # Automatically create missing paths
         # parser stuff
-        self.start = kwargs.pop("STARTDIR", None) # Let defaults be set by argparse
-        self.older = kwargs.pop("OLDER", None) # Let defaults be set by argparse
-        self.newer = kwargs.pop("NEWER", None) # Let defaults be set by argparse
-        self.outfile = kwargs.pop("OUTFILE", None) # Let defaults be set by argparse
-        self.MAXDEPTH = kwargs.pop("MAXDEPTH", None) # Let defaults be set by argparse
+        self.startdir = kwargs.pop("STARTDIR", ".") 
+        self.older = kwargs.pop("OLDER", 0)
+        self.newer = kwargs.pop("NEWER", 0)
+        self.outfile = kwargs.pop("OUTFILE", None)
+        self.MAXDEPTH = kwargs.pop("MAXDEPTH", 0)
+        self.FILETYPE = kwargs.pop("FILETYPE", "b")
+        self.TERMINAL = kwargs.pop("TERMINAL", False)
+        self.INCREMENTOUT = kwargs.pop("INCREMENTOUT", "s")
         
         # Everything else goes into the conf
         #==== # confighandler Needs to be updated for Python3 ==================
@@ -158,6 +169,20 @@ class Find(object):
                  )
 
     @property
+    def increment_readable(self):
+        try: 
+            if   self.INCREMENTOUT.lower().startswith("y"): return "Year(s)" 
+            elif self.INCREMENTOUT.startswith("M") or self.INCREMENTOUT.lower().startswith("mo"):  return "Month(s)"
+            elif self.INCREMENTOUT.lower().startswith("d"): return "Day(s)" 
+            elif self.INCREMENTOUT.startswith("h") or self.INCREMENTOUT.lower().startswith("ho"): return "Hour(s)"
+            elif self.INCREMENTOUT.lower() == "m"  or self.INCREMENTOUT.lower().startswith("mi"): return "Minute(s)"
+            elif self.INCREMENTOUT.lower().startswith("s") or len(self.INCREMENTOUT) < 1: return "Second(s)"
+            elif self.INCREMENTOUT.startswith("H") or self.INCREMENTOUT.lower().startswith("hu"): return ""
+            else: return "no match"
+        except Exception as e: 
+            return str(e)
+        
+    @property
     def startdir(self):
         try:
             return self.STARTDIR # Just the path name
@@ -167,6 +192,11 @@ class Find(object):
         
     @startdir.setter
     def startdir(self, value):
+        # Make full path
+        if value.startswith('.'):
+            _current = os.getcwd()
+            value = value.replace(".", _current, 1)
+            
         try:
             _value = str(value)
             # Strip extra delimitors
@@ -182,6 +212,8 @@ class Find(object):
         except Exception as e:
             err = "The {A} ({V}) does not appear to exist or cannot be recognized. ".format(A = str(inspect.stack()[0][3]), V = str(value))
             raise ValueError(err)
+        
+        print("self.STARTDIR=", self.STARTDIR)
         
     @startdir.deleter
     def startdir(self):
@@ -217,12 +249,22 @@ class Find(object):
         
     @outfile.setter
     def outfile(self, value):
-        if value is None: 
+        print()
+        print()
+        print("outfile.value=", value)
+
+        if "none" in str(value).lower():
             self.OUTFILE = None
+            self._outfile = None
             return 
         
+
+        if value.startswith('.'):
+            _current = os.getcwd()
+            value = value.replace(".", _current, 1)
+        
         try:
-            self._outfile = open(value, "w+")
+            self._outfile = open(value, "w")
             self.OUTFILE = value
         except Exception as e:
             err = "Unable to open {F}. ({E})".format(F = str(value), E = str(e))
@@ -244,7 +286,7 @@ class Find(object):
     @older.setter
     def older(self, value):
         try:
-            self.OLDER = get_seconds(value, 's')
+            self.OLDER = get_time(value, 's')
         except ValueError as e:
             err = "Failed to set attribute '{P}' with '{V}'. ({E})".format(P = str(inspect.stack()[0][3]), V = str(value), E = str(e))
             raise ValueError(err)
@@ -264,7 +306,7 @@ class Find(object):
     @newer.setter
     def newer(self, value):
         try:
-            self.NEWER = get_seconds(value, "s")
+            self.NEWER = get_time(value, "s")
         except ValueError as e:
             err = "Failed to set attribute '{P}' with '{V}'. ({E})".format(P = str(inspect.stack()[0][3]), V = str(value), E = str(e))
             raise ValueError(err)
@@ -274,18 +316,58 @@ class Find(object):
         del self.NEWER
 
     def walkit(self):
-        self.WALK = {}
-        for root, dirs, files in os.walk(".", topdown=True):
-            
-
+        """"""
+        self.results = []
+        _now = time.time()
+        for root, dirs, files in os.walk(self.startdir):
+            # Check depth RELATIVE TO root
+            dir_depth = root.replace(self.startdir, "") # Remove relative root
+            dir_depth = dir_depth.split(checks.directory_deliminator())
+            dir_depth = [x for x in dir_depth if len(x) > 1]
+            dir_depth = len(dir_depth) + 1 # Start at 1 not 0
         
+            if (dir_depth <= self.maxdepth) or (self.maxdepth == 0):
+                _dir_youngest_time = os.stat(root).st_mtime # Reset at each root loop
+        
+                for fn in files:
+                    path = os.path.join(root, fn)
+                    try:
+                        _time = os.stat(path).st_mtime # in epoch
+                    except Exception as e:
+                        message = "Error gathering mtime from path {P}. Skipping. (ERROR: {E})".format(P = path, E = str(e))
+                        log.error(message)
+                        self.results.append([message])
+                        if self.TERMINAL: print(message)
+                        if self._outfile is not None: self._outfile.write(str([message]) + "\n")
+                        continue
+                    
+                    _diff = _now - _time
+                    # Set the directory time to the youngest file in the dir
+                    if _time > _dir_youngest_time: _dir_youngest_time = _time
+                    # If it matches the input time rnge
+                    if ((_diff >= self.older) or (self.older == 0)) and ((_diff <= self.newer) or (self.newer == 0)):
+                         # If individual file listings was set
+                         if ("f" in self.FILETYPE.lower()) or ("b" in self.FILETYPE.lower()):
+                             _append = [path, get_time(int(_diff), self.INCREMENTOUT), self.increment_readable, _time]
+                             self.results.append(_append)  
+                             if self.TERMINAL: print(_append)
+                             if self._outfile is not None: self._outfile.write(str(_append) + "\n")
+                # If directorys was set
+                if ("d" in self.FILETYPE.lower()) or ("b" in self.FILETYPE.lower()):
+                    _diff = _now - _dir_youngest_time
+                    _append = [root, get_time(int(_diff), self.INCREMENTOUT), self.increment_readable, _time]
+                    self.results.append(_append)  
+                    if self.TERMINAL: print(_append)
+                    if self._outfile is not None: self._outfile.write(str(_append) + "\n")
+                
     def main(self):
         """"""
         log.debug("Running 'Find' with parameters: {D}".format(D = str(self.__dict__)))
+        self.walkit()
         log.debug("Done.")
         
     
 if __name__ == '__main__':
     parser = ArgumentParser()
     object = Find(parser)
-    print(object.older)
+    object.main()
--- test.py
+++ test.py
@@ -8,40 +8,53 @@ import os
 import time
 
 startdir = "/Users/mikes/Documents/Work/Heidelberg/projects/BareOS/bareos-virtualenv/bareos-scripts"
-maxdepth = 3
+maxdepth = 0
 results = []
 older = 0
 newer = 0
-FILETYPE = 'd'
+FILETYPE = 'b'
+_outfile = "STDOUT"
+INCREMENTOUT = "h"
+TERMINAL = True
+_outfile = open("./deleteme.out", "w")
 
 _now = time.time()
 for root, dirs, files in os.walk(startdir):
-    dir_youngest_time = 0 # Reset at each root loop
     # Check depth RELATIVE TO root
     dir_depth = root.replace(startdir, "") # Remove relative root
     dir_depth = dir_depth.split(checks.directory_deliminator())
     dir_depth = [x for x in dir_depth if len(x) > 1]
     dir_depth = len(dir_depth) + 1 # Start at 1 not 0
-    if dir_depth <= maxdepth: 
+
+    if (dir_depth <= maxdepth) or (maxdepth == 0):
+        _dir_youngest_time = os.stat(root).st_mtime # Reset at each root loop
+
         for fn in files:
             path = os.path.join(root, fn)
             _time = os.stat(path).st_mtime # in epoch
             _diff = _now - _time
-#             print("_diff=", _diff) #333
+            # Set the directory time to the youngest file in the dir
+            if _time > _dir_youngest_time: _dir_youngest_time = _time
+            # If it matches the input time rnge
             if ((_diff >= older) or (older == 0)) and ((_diff <= newer) or (newer == 0)):
-                 if _diff < dir_youngest_time:  dir_youngest_time = _diff
+                 # If individual file listings was set
                  if ("f" in FILETYPE.lower()) or ("b" in FILETYPE.lower()):
-                     results.append([path, human_time(int(_diff), "h"), "hours"])  
-                     print(path, ":", time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(_time)), human_time(int(_diff), "h"),"hours")
-                     print("dir_youngest_time=", dir_youngest_time)
-                     
-        if "d" in FILETYPE.lower():
-            results.append([root, human_time(int(dir_youngest_time), "h"), "hours"])  
-            print(path, ":", time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(_time)), human_time(int(_diff), "h"), "hours")
+                     _append = [path, human_time(int(_diff), INCREMENTOUT), INCREMENTOUT]
+                     results.append(_append)  
+                     if TERMINAL: print(_append)
+                     if _outfile is not None: _outfile.write(str(_append))
+        # If directorys was set
+        if ("d" in FILETYPE.lower()) or ("b" in FILETYPE.lower()):
+            _diff = _now - _dir_youngest_time
+            _append = [root, human_time(int(_diff), INCREMENTOUT), INCREMENTOUT]
+            results.append(_append)  
+            if TERMINAL: print(_append)
+            if _outfile is not None: _outfile.write(str(_append))
 
-    else: 
-        print("dir_depth = {D}..Skipping".format(D=dir_depth))
-        
+#     else: 
+#         print("dir_depth = {D}..Skipping".format(D=dir_depth))
+
+_outfile.close()        
 print(results)
 # if __name__ == "__main__":
     
