67fd8d6e034b07313783bb0c1a2812c8ec36827e
Mike Rightmire <Mike.Rightmire@BiocomSoftware.com>
2017-10-07 09:14:56 +0200

    2017-10-7 MPR - Completed workable fundArchiveLinks script. Needs some fine tuning

--- findArchiveDiskLinks.py
+++ findArchiveDiskLinks.py
@@ -17,13 +17,16 @@ from argparse       import ArgumentParser
 # from common.confighandler  import ConfigHandler # Disabled until updated to py3
 # from loghandler     import log # Disabled until bug-fixed
 from common.checks         import Checks
-from common.directory_tools import findLinks
-
 checks = Checks()
 _delim = checks.directory_deliminator()
+from common.directory_tools import findLinks
+from common.runsubprocess import RunSubprocess as run
 
 from inspect import stack
 # import inspect
+import atexit
+# Put an exist dump of all searcch information and critical dicts
+import ntpath
 import os
 
 class FindArchiveDiskLinks(object):
@@ -85,7 +88,7 @@ class FindArchiveDiskLinks(object):
         value = list(set(value))
         # Check values
         for dir in value:
-            if not isinstance(dir, str) or not value.startswith(_delim): # Must be full path
+            if not isinstance(dir, str) or not dir.startswith(_delim): # Must be full path
                 raise ValueError(err.format(A = str(stack()[0][3]), V = str(value)))
             # Add end slash if needed
             elif not dir.endswith(_delim):
@@ -116,7 +119,7 @@ class FindArchiveDiskLinks(object):
             
         """
         parser.add_argument('--directory', '-d', action="store", dest="START_DIR", type=str, default = None, help='Starting directory for search.')
-        parser.add_argument('--search_dirs', '-D', action="store", dest="SEARCH_DIRS", action='append', nargs='*', default = ["/beegfs", "phobos:/mnt/group_cd"], help='Directories in which to search for missing files. ')
+        parser.add_argument('--search_dirs', '-D', action="append", dest="SEARCH_DIRS", nargs='*', default = ["/beegfs", "/mnt/group_cd"], help='Directories in which to search for missing files. ')
         return parser
     
     def _set_config(self, parser, args, kwargs):
@@ -153,7 +156,8 @@ class FindArchiveDiskLinks(object):
         self.start_dir = kwargs.pop("START_DIR", None)
         self.search_dirs = kwargs.pop("SEARCH_DIRS", None)
         self._yes_exists = {}
-        self._no_exists = {}
+        self._no_exists  = {}
+        self.searches    = {}
         # Everything else goes into the conf
         for key, value in kwargs.items():
             self.CONF.set(key, value)
@@ -214,11 +218,52 @@ class FindArchiveDiskLinks(object):
         
         return result
         
-    def score_found_by_path(self):
+    def score_found_by_path(self, orig, found):
         """
         Scores the found file against original path
+        MUST RETURN A DICT {found:<int>}
+        Go backwards, 0 for a match, -1 for a mismatch
+        
+        A match of [0,0,0,0,0] is exact, so we can assume it's the right file
+        
+        Matches like [-1, 0, 0, 0, 0] COULD be the right file, since it could
+        be something like...
+        /Users/mikes/Documents/tmp/test.c
+        versus
+        /archive/lab/Users/mikes/Documents/tmp/test.c
+        
+        The longer the match FROM the RIGHT, the more likely it's correct. 
+        [-1, 0, 0, 0, 0, 0, 0, 0]
+        much more likely than 
+        [-1, 0]
+        
+        A match like [0,-1,0,0] (a mismatch in the middle) is probably NOT the 
+        right file, since this is likely a dramatic difference like...
+        /Users/mikes/Documents/tmp/test.c
+        versus
+        /Users/tim/Documents/tmp/test.c
         """
-        pass
+        # Score
+        # Super simple matching_dirs/total/dirs
+        # be sure to ignore empty list item (sometimes extra directory delims are reported)
+        orig_l  = orig.split(_delim)
+        found_l = found.split(_delim)
+        score_l = []
+        # Go backwards, 0 for a match, -1 for a mismatch
+        count = len(orig_l) if len(orig_l) > len(found_l) else len(found_l)
+        for i in range(1, count, 1):
+            try:    orig_w = orig_l[len(orig_l)-i]
+            except: orig_w = ""
+            try:    found_w = found_l[len(found_l)-i]
+            except: found_w = ""
+            if orig_w == found_w: score_l = [0]  + score_l
+            else                : score_l = [-1] + score_l
+        print(found, ":", score_l) #333
+        autoset = False
+        if sum(score_l) == 0:
+            autoset = True
+            print("Pretending to make the link here...")
+        return {found:[score_l, {"automatically linked": autoset}]}
         
     def search(self, path):
         """
@@ -226,17 +271,54 @@ class FindArchiveDiskLinks(object):
         The try to score it against the original file
         """
         #  path must be a full path, strip to name
-        all_found = {} # results of all finds
-        for search_path in self-search_dirs:
-            pass
+        _path = str(path)
+        if not _path.startswith(_delim): 
+            err = "{C}.{M}: Path '{P}' does not seem to be a full path.".format(C = self.__class__.__name__, M = stack()[0][3], P = _path)
+#             raise ValueError(err)
+            print(err)
+            return [] 
+        
+        if _path.endswith(_delim):
+            err = "{C}.{M}: Path '{P}' does not seem to end in a filename.".format(C = self.__class__.__name__, M = stack()[0][3], P = _path)
+#             raise ValueError(err)
+            print(err)
+            return [] 
+        
+        _dir  = ntpath.dirname(_path)
+        _file = ntpath.basename(_path)
+        all_found    = [] # results of all finds
+        scored_found = {}
+        for search_path in self.search_dirs:
+            command = ["find", search_path, "-type", "f",  "-name", _file]
+            _result = run(command, output = "list")
+            result = []
+            for path in _result:
+                path = _delim + _delim.join(i for i in path.split(_delim) if len(i) > 0)
+                result.append(path)
+            # Sometimes extraneous directory delimiters sneak in. Remove
+            
+            all_found += result 
             # Use subprocess to do a find for filename in each search dir
             # Add founds (full path) to all_found
             # Score the found path against the original path to see if they are similar. 
             # Add all_found[original path] = {found_path:score}
-        
-            
+        for found in all_found:
+            result = self.score_found_by_path(_path, found)
+            scored_found.update(result)
+            # Try/except defacto checks for first addition of a list
+            try:
+                if self.searches[_path]: # list item already exists
+                     self.searches[_path].append(result)
+            except KeyError as e:
+                self.searches[_path] = [result]
+                    
     def main(self):
         """"""
+#         self.search_dirs = ["/mnt/group_cd"]
+        ########################################################
+        ### Kluge. This should be input not hard coded, fix #333 
+        self.search_dirs = ["/mnt/group_cd", "/beegfs/prj"]
+        ########################################################
         # Results is a dict, key = the original link path
         self.results = {}
         while self.start_dir is None:
@@ -248,27 +330,36 @@ class FindArchiveDiskLinks(object):
                 err = "Invalid input ({E}). \nTry again.".format(E = str(e))
                 print(err)
             
-        for dir, link in findLinks(self.start_dir):
-#         for dir, link in findLinks(self.start_dir, use = "os"):
-            print("dir:", dir)
-            print("Link:", link)
+        for dir, link in findLinks(self.start_dir, use = "os"):
             result = self.exists(link)
-            print(result)
             if result["exists"]:
+                print("The link: {L} does exist. Moving on".format(L = link)) 
                 self._yes_exists[dir] = result
             else:
-                self._no_exists[dir] = result
+                print()
+                print("=================")
+                print ("link: {L} does not exist. Searching...".format(L = link))
+                self.search(link)
         
+        import pickle
+        import time
+        with open('/home/mrightmire/tmp/findArchiveDiskLinks-' + str(time.time()) + ".out", 'wb') as f:
+            pickle.dump(self.searches, f)
+        
+#===============================================================================
+#         for key, value in self.searches.items():
+#             print("======================")
+#             print(key)
+#             for i in value:
+# #                 if "True" in str(i):
+#                 print("...", i)   
+#===============================================================================
+                                
+                #self._no_exists[dir]  = result
         # Now we have all the links as existing or not. 
         # Next we need to confirm if the 'existing' links make sense
 #         self.exists_sanity_check() # Will add impossibly 'exists' to the no_exists    
         # Search for the missing files. 
-                
-        print("YES======")
-        print(self._yes_exists)
-        print()
-        print("NO--------")
-        print(self._no_exists)
 #         log.debug("Done.")
         
     
